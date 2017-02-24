f8f07e9a5e5b527d53e0035cc6e53574278bc605
ertugrulkucukali <ertugrulkucukali@gmail.com>
2017-02-24 16:04:32 +0200

    Update Maksimum_Altkume_Toplami_1.py

--- Maksimum_Altkume_Toplami_1.py
+++ Maksimum_Altkume_Toplami_1.py
@@ -1,11 +1,12 @@
 def maxSubSum1(myvector):
     maxSum=0
     for i in range(len(myvector)):
-        thisSum=0
         for j in range(i,len(myvector)):
-            thisSum=thisSum+myarray[k]
-            if (thisSum>maxSum):
-                maxSum=thisSum
+            thisSum=0
+            for k in range (i,j):
+                thisSum=thisSum+myarray[j]
+                if (thisSum>maxSum):
+                    maxSum=thisSum
     return maxSum
 
 
