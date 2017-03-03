2c58c3ced9a25ed26c8a91c1a72ca492dc445b71
ertugrulkucukali <ertugrulkucukali@gmail.com>
2017-03-03 13:11:26 +0200

    Update Us_Alma_Log_N.py

--- Us_Alma_Log_N.py
+++ Us_Alma_Log_N.py
@@ -1,17 +1,19 @@
+s=0
 def pow_log_n(x,n):
     """karmaşıklık = log n"""
-    
+    global s
     if(n==0):
+        s=s+1
         return 1
     elif(n==1):
+        s=s+1
         return x
     elif(n%2==0):
-        
+        s=s+1
         return pow(x*x,int(n/2))
     else:
-       
+        s=s+1
         return pow(x*x,int(n/2))*x
-        
 
 
-pow_log_n(2,60)
+pow_log_n(2,60),s
